02aa04e0e882e922bbf451b90c7a533266c33794
gitaukevin <kevingitau17@gmail.com>
2016-10-12 16:50:06 +0300

    Binary search function

---
+++ Binary_search.py
@@ -0,0 +1,28 @@
+class List(object):
+
+	one_to_twenty=range(20,1)
+	one_to_forty=range(20,2)
+	one_to_thousand=range(100,10)
+
+	def __init__(self,a,b):
+		self.a=a
+		self.b=b
+		a=len(List)
+		b=i-k
+		lenght={}
+	def search(self):
+		ans=length.append(count(index))
+		pass
+	def BinarySearch(List):
+	    if self.one_to_twenty not in range(20,1):
+	        return None
+	    else:
+	        return ans
+	    if self.one_to_forty not in range(20,2):
+	        return None
+	    else:
+	        return ans
+	    if self.one_to_thousand not in range(100,10):
+	        return None
+	    else:
+	        return ans        	
